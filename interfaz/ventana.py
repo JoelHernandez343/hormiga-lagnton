f2a49a21e78cd7892b5cb0436dcf49d698065a1a
JoelHernandez343 <joel.programador@gmail.com>
2021-02-05 17:11:02 -0600

    Empezando controles

--- interfaz/ventana.py
+++ interfaz/ventana.py
@@ -8,22 +8,22 @@ class Ventana:
         self.root.geometry(tam)
 
         # crear el layout
-        visor = tk.Frame(master=self.root, width=400, height=1000, bg="red")
+        visor = tk.Frame(master=self.root, width=400, height=1000, bg="gray")
         visor.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
 
-        controles = tk.Frame(master=self.root, width=300, height=1000, bg="blue")
+        controles = tk.Frame(master=self.root, width=300, height=1000, bg="gray")
         controles.pack(fill=tk.BOTH, side=tk.LEFT, expand=False)
 
         self.hacer_controles(controles)
 
     def hacer_controles(self, controles):
 
-        contenedor = tk.Frame(master=controles, width=300, height=1000, bg="yellow")
+        contenedor = tk.Frame(master=controles, width=300, height=1000, bg="blue")
         contenedor.pack(fill=tk.BOTH, side=tk.LEFT, expand=False, padx=20, pady=20)
         contenedor.pack_propagate(0)
 
-        lbl = tk.Label(master=contenedor, text="Estos son los controles")
-        lbl.pack()
+        lbl = tk.Label(master=contenedor, text="Elegir variante")
+        lbl.pack(side=tk.TOP, anchor=tk.NW)
 
     def ejecutar(self):
         self.root.mainloop()
